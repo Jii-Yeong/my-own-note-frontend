7d77d20e5f476c5cf116f36f21f2b69333dd6812
Ji Yeong Seo <tj5134@naver.com>
2022-01-10 22:49:05 +0900

    TextArea component 정보만 전달하도록 수정

--- src/component/TextArea/index.tsx
+++ src/component/TextArea/index.tsx
@@ -1,9 +1,9 @@
-import React, { RefObject, useEffect } from "react";
+import React, { KeyboardEvent, RefObject } from "react";
 import styled from "styled-components";
 
 type Props = {
   textAreaRef: RefObject<HTMLTextAreaElement>;
-  changeTextArea: () => void;
+  changeTextArea: (e: KeyboardEvent<HTMLTextAreaElement>) => void;
 }
 
 const Wrapper = styled.textarea`
@@ -11,18 +11,15 @@ const Wrapper = styled.textarea`
   height: 1000px;
   border: none;
   font-size: 20px;
+  position: absolute;
   &:focus {
     outline: none;
   }
 `;
 
 const TextArea = ({ textAreaRef, changeTextArea }: Props) => {
-  useEffect(() => {
-    console.log("gg", textAreaRef.current?.value);
-  }, [textAreaRef.current?.value]);
   return (
-    <Wrapper ref={textAreaRef} onChange={changeTextArea}>
-
+    <Wrapper ref={textAreaRef} onKeyUp={(e) => changeTextArea(e)}>
     </Wrapper>
   )
 }
