f5b5404d2041c10b1424d93a43698b2e734a9910
Ji Yeong Seo <tj5134@naver.com>
2022-01-12 01:06:24 +0900

    에디터 태그를 div 방식에 맞춰 로직 수정

--- src/component/MainPanel/index.tsx
+++ src/component/MainPanel/index.tsx
@@ -1,5 +1,6 @@
 import TextArea from "$src/component/TextArea";
 import TextModal from "$src/component/TextModal";
+import { convertHtmlElements, NBSP, SPACE } from "$src/util/convert";
 import React, { KeyboardEvent, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
@@ -7,65 +8,59 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
 `
-
 const MainPanel = () => {
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const [textContent, setTextContent] : [string, any] = useState("");
-  const [splitContent, setSplitContent] : [Array<string>, any] = useState([]);
-  const [keyEventObject, setKeyEventObject] : [any, any] = useState({});
-  let [lineNumber, setLineNumber] : [number, any] = useState(0);
+  const textAreaRef = useRef<HTMLDivElement>(null);
+  const [keyEventObject, setKeyEventObject]: [any, any] = useState({});
+  let [lineNumber, setLineNumber]: [number, any] = useState(0);
 
-  const [isOpenTextModal, setOpenTextModal] : [boolean, any] = useState(false);
+  const [isOpenTextModal, setOpenTextModal]: [boolean, any] = useState(false);
 
-  const handleChangeTextareaValue = (e : KeyboardEvent<HTMLTextAreaElement>) => {
+  const handleChangeTextareaValue = (e: KeyboardEvent<HTMLDivElement>) => {
     setKeyEventObject(e);
-    const value = textAreaRef.current?.value;
-    setSplitContent(value?.split('\n'));
   }
 
   useEffect(() => {
-    const keyName = keyEventObject.key;
-    
-    if (keyName === "Enter") {
-      setLineNumber(++lineNumber);
-    }
-    if (isOpenTextModal) {
-      if (keyName === "Enter") {
-        setOpenTextModal(false);
-        splitContent.pop();
-        setTextContent(splitContent.join(""));
-        if (textAreaRef.current) {
-          splitContent.push("");
-          textAreaRef.current.value = splitContent.join("\n");
-          setLineNumber(--lineNumber);
-        }
+    if (textAreaRef.current) {
+      const childNodes = textAreaRef.current.childNodes;
+
+      if (childNodes?.length === 1 || childNodes?.length === 0) {
+        setLineNumber(0);
+      } else {
+        setLineNumber(childNodes?.length - 1);
       }
-    }
-    if (keyName === 'Backspace') {
-      if (splitContent[splitContent.length - 1] === '') {
-        if (lineNumber <= 0) {
-          setLineNumber(0);
-        } else {
-          setLineNumber(--lineNumber);
-        }
+
+      const makeTextLine = childNodes[lineNumber] as HTMLElement;
+      const textLineParentNode = makeTextLine?.parentNode as HTMLElement;
+      const textLineLastChild = makeTextLine?.lastChild as HTMLElement;
+
+      convertHtmlElements(makeTextLine, textLineParentNode, textLineLastChild);
+
+      if (textLineParentNode?.innerHTML === `/${NBSP}`) {
+        setOpenTextModal(true);
+      } else {
         setOpenTextModal(false);
       }
-      if (splitContent[splitContent.length - 1].length === 1) {
+
+      if (makeTextLine?.innerText === `/${SPACE}`) {
+        setOpenTextModal(true);
+      } else {
         setOpenTextModal(false);
       }
+
+      if (makeTextLine?.innerText?.slice(-2, -1) === "/") {
+        setOpenTextModal(true);
+      } else {
+        setOpenTextModal(false);
+      }
+      
     }
-    const makeTextLine = splitContent[lineNumber];
-    if (makeTextLine === "/ ") {
-      setOpenTextModal(true);
-    }
-    console.log(splitContent, splitContent[lineNumber], lineNumber);
-  }, [splitContent]);
+  }, [textAreaRef.current?.innerHTML]);
 
   return (
-  <Wrapper>
-    <TextArea textAreaRef={textAreaRef} changeTextArea={handleChangeTextareaValue} />
-    {isOpenTextModal && <TextModal />}
-  </Wrapper>
+    <Wrapper>
+      {isOpenTextModal && <TextModal />}
+      <TextArea textAreaRef={textAreaRef} changeTextArea={handleChangeTextareaValue} />
+    </Wrapper>
   )
 }
 
