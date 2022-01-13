5a7a2080f5b88bc640cb330efe16c1196bef8602
Ji Yeong Seo <tj5134@naver.com>
2022-01-13 22:31:15 +0900

    글 내용의 한 줄을 바꾸는 reducer 함수 작성

--- src/stores/modules/textContentSlice.ts
+++ src/stores/modules/textContentSlice.ts
@@ -6,13 +6,20 @@ import { createSlice, SliceCaseReducers } from "@reduxjs/toolkit";
    reducers: {
      setTextContents(_, action) {
       const { contents } = action.payload;
+      console.log("contents", contents);
       const redefineContents: Array<string> = [...contents];
+      console.log("redefineContents", redefineContents);
       return redefineContents;
-     },
+    },
 
      addTextContents(state, action) {
        const { textLine } = action.payload;
        state.push(textLine);
+    },
+
+     changeTextContetns(state, action) {
+       const { lineNumber, textLine } = action.payload;
+       state.splice(lineNumber, 1, textLine);
      },
      
      removeTextContents(state) {
@@ -21,6 +28,6 @@ import { createSlice, SliceCaseReducers } from "@reduxjs/toolkit";
    }
  });
 
-export const { setTextContents, addTextContents, removeTextContents } = textContentSlice.actions;
+export const { setTextContents, addTextContents, removeTextContents, changeTextContetns } = textContentSlice.actions;
 
 export default textContentSlice.reducer;
