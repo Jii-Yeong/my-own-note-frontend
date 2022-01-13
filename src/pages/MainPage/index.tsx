66101a073b8bb5066df98b03ed47a7569170b843
Ji Yeong Seo <tj5134@naver.com>
2022-01-13 21:17:01 +0900

    redux 사용을 위한 Provider 태그 설정

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,10 +1,15 @@
 import MainPanel from "$src/component/MainPanel";
+import reducers from "$src/stores/reducers";
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./configureStore";
 
 const MainPage = () => {
 
   return (
-    <MainPanel />
+    <Provider store={store}>
+      <MainPanel />
+    </Provider>
   )
 }
 
