1b5ff7937b412bfaf4e7f6e51cf8ad0582e98f21
Ji Yeong Seo <tj5134@naver.com>
2022-01-16 13:36:26 +0900

    메인 페이지 flex 적용을 위한 div 추가

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,14 +1,23 @@
 import MainPanel from "$src/component/MainPanel";
-import reducers from "$src/stores/reducers";
+import SidePanel from "$src/component/SidePanel";
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./configureStore";
+import styled from "styled-components";
+import store from "./configureStore";
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+`
 
 const MainPage = () => {
-
   return (
     <Provider store={store}>
-      <MainPanel />
+      <Wrapper>
+        <SidePanel />
+        <MainPanel />
+      </Wrapper>
     </Provider>
   )
 }
