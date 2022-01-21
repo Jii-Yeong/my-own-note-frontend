d493853aae86b01b6a17077db12b556a76aacff7
Ji Yeong Seo <tj5134@naver.com>
2022-01-21 23:46:53 +0900

    redux-persist 적용

--- src/pages/MainPage/configureStore.ts
+++ src/pages/MainPage/configureStore.ts
@@ -1,12 +1,14 @@
-import rootReducer from "$src/stores/reducers";
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
+import persistedReducer from "$src/stores/reducers";
+import persistStore from "redux-persist/es/persistStore";
+import thunk from "redux-thunk";
 
-const store = configureStore({
-  reducer: rootReducer,
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
 })
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-export default store;
--- src/stores/reducers/index.ts
+++ src/stores/reducers/index.ts
@@ -1,10 +1,22 @@
+/// <reference types="redux-persist" />
+
 import { combineReducers } from "@reduxjs/toolkit";
 import textContentsReducer from "../modules/textContentSlice";
 import pageSliceReducer from "../modules/pageSlice";
+import userSliceReducer from '../modules/userSlice';
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ["userInfo"]
+};
 
 const rootReducer = combineReducers({
   textContent: textContentsReducer,
   page: pageSliceReducer,
+  userInfo: userSliceReducer,
 })
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
