4f62660e7141271385f8546a985b03019e940b3e
Ji Yeong Seo <tj5134@naver.com>
2022-01-22 19:18:47 +0900

    페이지 api 파일 생성 및 필요없는 api 파일 삭제

---
+++ src/service/page.api.ts
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { Page } from "./types/page"
+
+export const getAllPageList = (userId: string) => {
+  const data = {
+    userId: userId
+  }
+  return axios.post('/api/page/select/all', data).then(response => response.data);
+};
+
+export const addPage = (pageInfo: Page) => {
+  return axios.post('api/page/insert', pageInfo).then(response => response.data);
+}
+
+export const getPageContent = (pageId: number, title: string) => {
+  const data = {
+    pageId: pageId,
+    title: title,
+  }
+  return axios.post('/api/page/select/id', data).then(response => response.data);
+}
+
+export const addPageContent = (pageId: number, text: string, index: number) => {
+  const data = {
+    pageId: pageId ?? 0,
+    text: text,
+    index: index ?? 0,
+  }
+  console.log("data",data);
+  return axios.post('api/page/insert/content', data).then(response => response.data);
+}
+
+export const removePageContentToIndex = (index: number) => {
+  return axios.delete(`api/page/delete/index?index=${index}`).then(response => response.data);
+}
