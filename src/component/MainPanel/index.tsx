a5e61e9fc720019c260c949ff54d4a038050d1eb
Ji Yeong Seo <tj5134@naver.com>
2022-01-22 20:39:27 +0900

    입력 이벤트 함수에 쓰로틀링 설정 및 input value 전체 저장 api 호출

--- src/component/MainPanel/index.tsx
+++ src/component/MainPanel/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SLICE_REGEX } from "$src/util/constant";
 import { convertHtmlElements, convertInputValue } from "$src/util/convert";
 import styled from "styled-components";
@@ -7,24 +7,23 @@ import ContentBox from "../ContentBox";
 import TextModal from "../TextModal";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "$src/stores/types/root";
-import { deletePageContentToIndex, insertPageContent } from "$src/stores/modules/pageSlice";
 import useDom, { handleDragOverElement } from "$src/pages/MainPage/hooks/dom";
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
 `
+let timer: NodeJS.Timeout | null;
 
 const MainPanel = () => {
   const divRef = React.createRef() as React.RefObject<HTMLDivElement>;
   const inputWrapperRef = React.createRef() as React.RefObject<HTMLDivElement>;
-  const inputRef = useRef([]);
   const [styleObject, setStyleObject] = useState<{ [key: string]: any }>({});
   const [isOpenTextModal, setOpenTextModalState] = useState<boolean>(false);
   const [currentInputEl, setCurrentInputEl] = useState<HTMLInputElement>();
   const pageContent = useSelector((state: RootState) => state.page.pageContent);
   const currentPageId = useSelector((state: RootState) => state.page.currentPageId);
-  const { createInputEl, insertInpulElToMiddleInput, insertInputElToLastInput } = useDom();
+  const { createInputEl, insertInpulElToMiddleInput, insertInputElToLastInput, saveInputAllContent } = useDom();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -40,12 +39,12 @@ const MainPanel = () => {
       wrapper?.removeChild(div);
     })
     if (pageContent.text) {
-      pageContent.text.forEach((content: {[key: string]: any}, index: number) => {
-        const divEl = createInputEl(handleInputKeyUp, content.text, inputRef, index) as HTMLElement;
+      pageContent.text.forEach((content: { [key: string]: any }) => {
+        const divEl = createInputEl(handleInputKeyUp, content.text) as HTMLElement;
         wrapper?.prepend(divEl);
       })
     }
-  }, [pageContent, inputRef]);
+  }, [pageContent]);
 
   const handleInputKeyUp = (e: KeyboardEvent | React.KeyboardEvent<HTMLElement>) => {
     const currentTarget = e.target as HTMLInputElement;
@@ -53,6 +52,7 @@ const MainPanel = () => {
     const nextParentNode = parentNode.nextSibling as HTMLElement;
     const nextTarget = parentNode.nextSibling?.firstChild as HTMLElement;
     const prevTarget = parentNode.previousSibling?.firstChild as HTMLElement;
+    const wrapper = document.getElementById('wrapper');
 
     if (e.key === 'Enter') {
       insertInpulElToMiddleInput(handleInputKeyUp, parentNode);
@@ -75,18 +75,20 @@ const MainPanel = () => {
     if (e.key === 'Backspace' && currentTarget.selectionStart === 0 && prevTarget) {
       prevTarget.focus();
       parentNode.remove();
+      if (wrapper) {
+        saveInputAllContent(wrapper, currentPageId);
+      }
     }
-    convertInputValue(currentTarget, setStyleObject);
-    const text = currentTarget.value;
-    console.log("parentNode.parentNode?.children", parentNode.parentNode?.children);
-    if (parentNode.parentNode?.children) {
-      const index = Array.prototype.indexOf.call(parentNode.parentNode?.children , parentNode);
-      dispatch(insertPageContent({ currentPageId, text, index }));
-    } else {
-      const index = Array.prototype.indexOf.call(nextParentNode.parentNode?.children , nextParentNode);
-      console.log("index", index);
-      dispatch(deletePageContentToIndex(index));
+
+    if (timer) {
+      clearTimeout(timer);
     }
+
+    timer = setTimeout(() => {
+      if (wrapper) {
+        saveInputAllContent(wrapper, currentPageId);
+      }
+    }, 600);
   }
 
   const handleClickTextList = (e: React.MouseEvent<HTMLElement>) => {
@@ -108,15 +110,13 @@ const MainPanel = () => {
 
   const handlePressEnterKey = (e: KeyboardEvent | React.KeyboardEvent<HTMLElement>) => {
     const currentTarget = e.target as HTMLInputElement;
-    const wrapper = divRef.current;
+    const wrapper = document.getElementById('wrapper');
+    console.log("wrapper", wrapper);
     if (e.key === 'Enter') {
       setStyleObject({});
       insertInputElToLastInput(handleInputKeyUp, inputWrapperRef, styleObject, divRef);
       if (wrapper) {
-        const prevTarget = currentTarget.parentNode?.previousSibling?.firstChild as HTMLInputElement;
-        const text = prevTarget.value;
-        const index = wrapper.children.length - 1;
-        dispatch(insertPageContent({currentPageId, text, index}));
+        saveInputAllContent(wrapper, currentPageId);
       }
     }
 
@@ -136,7 +136,7 @@ const MainPanel = () => {
   return (
     <Wrapper>
       {isOpenTextModal && <TextModal clickTextList={handleClickTextList} />}
-      <InputTitle title={pageContent.title ?? ''}/>
+      <InputTitle title={pageContent.title ?? ''} />
       <ContentBox
         divRef={divRef}
         inputWrapperRef={inputWrapperRef}
