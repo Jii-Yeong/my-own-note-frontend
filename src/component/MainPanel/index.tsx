922c017b36a10e4632e92b4174e06d8fd25f4100
Ji Yeong Seo <tj5134@naver.com>
2022-01-22 19:07:52 +0900

    글 입력 시 자동 저장되도록 redux api 호출

--- src/component/MainPanel/index.tsx
+++ src/component/MainPanel/index.tsx
@@ -1,105 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
 import { SLICE_REGEX } from "$src/util/constant";
 import { convertHtmlElements, convertInputValue } from "$src/util/convert";
-import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import ReactDOM from "react-dom";
 import InputTitle from "../InputTitle";
 import ContentBox from "../ContentBox";
 import TextModal from "../TextModal";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "$src/stores/types/root";
+import { deletePageContentToIndex, insertPageContent } from "$src/stores/modules/pageSlice";
+import useDom, { handleDragOverElement } from "$src/pages/MainPage/hooks/dom";
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
 `
 
-const Input = styled.input`
-  display: block;
-  height: 30px;
-  width: 93%;
-  margin: 0 auto;
-  border: none;
-  font-size: 15px;
-  font-family: 'nanum';
-  &: focus {
-    outline: none;
-  }
-`
-
 const MainPanel = () => {
   const divRef = React.createRef() as React.RefObject<HTMLDivElement>;
   const inputWrapperRef = React.createRef() as React.RefObject<HTMLDivElement>;
-  const [styleObject, setStyleObject] = useState<{[key: string]: any}>({});
+  const inputRef = useRef([]);
+  const [styleObject, setStyleObject] = useState<{ [key: string]: any }>({});
   const [isOpenTextModal, setOpenTextModalState] = useState<boolean>(false);
   const [currentInputEl, setCurrentInputEl] = useState<HTMLInputElement>();
+  const pageContent = useSelector((state: RootState) => state.page.pageContent);
+  const currentPageId = useSelector((state: RootState) => state.page.currentPageId);
+  const { createInputEl, insertInpulElToMiddleInput, insertInputElToLastInput } = useDom();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const startInput = inputWrapperRef.current?.firstChild as HTMLElement;
     startInput.focus();
   }, [])
 
-  const handleDragStartElement = (divEl: HTMLDivElement) => {
-    divEl.setAttribute("id", 'clicked');
-  }
-
-  const handleDragOverElement = (e: DragEvent | React.DragEvent<HTMLElement>) => {
-    const target = e.target as HTMLElement;
-    const firstChild = target.firstChild as HTMLInputElement;
-    if (target && target.tagName === 'DIV' && !firstChild.placeholder) {
-      e.preventDefault();
-      target.style.borderBottom = '2px solid #0000005f';
+  useEffect(() => {
+    const wrapper = divRef.current;
+    const wrapperNodeList = wrapper?.querySelectorAll('.new-div') as NodeList;
+    const newDivElList = Array.from(wrapperNodeList);
+    newDivElList.forEach(div => {
+      wrapper?.removeChild(div);
+    })
+    if (pageContent.text) {
+      pageContent.text.forEach((content: {[key: string]: any}, index: number) => {
+        const divEl = createInputEl(handleInputKeyUp, content.text, inputRef, index) as HTMLElement;
+        wrapper?.prepend(divEl);
+      })
     }
-  }
-
-  const handleDragLeaveElement = (e: DragEvent | React.DragEvent<HTMLElement>) => {
-    const target = e.target as HTMLElement;
-    target.style.borderBottom = 'none';
-  }
-
-  const handleDragEndElement = (e: DragEvent | React.DragEvent<HTMLElement>) => {
-    const target = e.target as HTMLElement;
-    target.style.borderBottom = 'none';
-  }
-
-  const handleDropElement = (e: DragEvent | React.DragEvent<HTMLElement>) => {
-    e.preventDefault();
-    const target = e.target as HTMLElement;
-    const currentTarget = e.target as HTMLElement;
-    const dragDiv = document.getElementById('clicked');
-    const dragInput = dragDiv?.firstChild?.cloneNode() as HTMLElement;
-    const dropInput = currentTarget.firstChild?.cloneNode() as HTMLElement;
-    if (dragInput && dropInput) {
-      dragInput.addEventListener('keyup', handleInputKeyUp);
-      dropInput.addEventListener('keyup', handleInputKeyUp);
-      dragDiv?.firstChild?.remove();
-      dragDiv?.appendChild(dropInput);
-      currentTarget.firstChild?.remove();
-      currentTarget.appendChild(dragInput);
-      dragDiv?.removeAttribute('id');
-    }
-    target.style.borderBottom = 'none';
-  }
+  }, [pageContent, inputRef]);
 
   const handleInputKeyUp = (e: KeyboardEvent | React.KeyboardEvent<HTMLElement>) => {
     const currentTarget = e.target as HTMLInputElement;
     const parentNode = currentTarget.parentNode as HTMLElement;
+    const nextParentNode = parentNode.nextSibling as HTMLElement;
     const nextTarget = parentNode.nextSibling?.firstChild as HTMLElement;
     const prevTarget = parentNode.previousSibling?.firstChild as HTMLElement;
-    
-    if (e.key === 'Enter') {
-      const divEl = document.createElement('div');
-      divEl.draggable = true;
-      divEl.addEventListener('dragstart', () => {
-        handleDragStartElement(divEl);
-      });
-      divEl.addEventListener('dragover', handleDragOverElement);
-      divEl.addEventListener('dragend', handleDragEndElement);
-      divEl.addEventListener('dragleave', handleDragLeaveElement);
-      divEl.addEventListener('drop', handleDropElement);
-      ReactDOM.render(<Input onKeyUp={(e) => handleInputKeyUp(e)} />, divEl);
-      parentNode.after(divEl);
 
-      const divFirstChild = divEl.firstChild as HTMLElement;
-      divFirstChild.focus();
+    if (e.key === 'Enter') {
+      insertInpulElToMiddleInput(handleInputKeyUp, parentNode);
     }
 
     if (e.key === 'ArrowDown') {
@@ -121,11 +77,21 @@ const MainPanel = () => {
       parentNode.remove();
     }
     convertInputValue(currentTarget, setStyleObject);
+    const text = currentTarget.value;
+    console.log("parentNode.parentNode?.children", parentNode.parentNode?.children);
+    if (parentNode.parentNode?.children) {
+      const index = Array.prototype.indexOf.call(parentNode.parentNode?.children , parentNode);
+      dispatch(insertPageContent({ currentPageId, text, index }));
+    } else {
+      const index = Array.prototype.indexOf.call(nextParentNode.parentNode?.children , nextParentNode);
+      console.log("index", index);
+      dispatch(deletePageContentToIndex(index));
+    }
   }
 
   const handleClickTextList = (e: React.MouseEvent<HTMLElement>) => {
     const currentTargetCommand = e.currentTarget.dataset.command as string;
-    const replaceText = convertHtmlElements('', currentTargetCommand) as {[key: string]: any};
+    const replaceText = convertHtmlElements('', currentTargetCommand) as { [key: string]: any };
     const styleList = Object.keys(replaceText);
     if (currentInputEl) {
       const currentInputStyle = currentInputEl?.style as { [key: string]: any };
@@ -142,29 +108,16 @@ const MainPanel = () => {
 
   const handlePressEnterKey = (e: KeyboardEvent | React.KeyboardEvent<HTMLElement>) => {
     const currentTarget = e.target as HTMLInputElement;
+    const wrapper = divRef.current;
     if (e.key === 'Enter') {
       setStyleObject({});
-      const divEl = document.createElement('div');
-      divEl.draggable = true;
-      divEl.addEventListener('dragstart', () => {
-        handleDragStartElement(divEl);
-      });
-      divEl.addEventListener('dragover', handleDragOverElement);
-      divEl.addEventListener('dragend', handleDragEndElement);
-      divEl.addEventListener('dragleave', handleDragLeaveElement);
-      divEl.addEventListener('drop', handleDropElement);
-      ReactDOM.render(<Input onKeyUp={(e) => handleInputKeyUp(e)} />, divEl);
-      let newInputEl = divEl.firstChild as HTMLInputElement
-      let oldInputEl = inputWrapperRef.current?.firstChild as HTMLInputElement;
-      newInputEl.value = oldInputEl.value;
-      oldInputEl.value = '';
-      const changeStyleList = Object.keys(styleObject) as Array<string>;
-      const newInputElStyle = newInputEl.style as {[key: string]: any};
-      changeStyleList.forEach(style => {
-        newInputElStyle[style] = styleObject[`${style}`];
-        oldInputEl.removeAttribute('style');
-      })
-      divRef.current?.insertBefore(divEl, inputWrapperRef.current);
+      insertInputElToLastInput(handleInputKeyUp, inputWrapperRef, styleObject, divRef);
+      if (wrapper) {
+        const prevTarget = currentTarget.parentNode?.previousSibling?.firstChild as HTMLInputElement;
+        const text = prevTarget.value;
+        const index = wrapper.children.length - 1;
+        dispatch(insertPageContent({currentPageId, text, index}));
+      }
     }
 
     if (e.key === 'ArrowUp') {
@@ -183,7 +136,7 @@ const MainPanel = () => {
   return (
     <Wrapper>
       {isOpenTextModal && <TextModal clickTextList={handleClickTextList} />}
-      <InputTitle />
+      <InputTitle title={pageContent.title ?? ''}/>
       <ContentBox
         divRef={divRef}
         inputWrapperRef={inputWrapperRef}
