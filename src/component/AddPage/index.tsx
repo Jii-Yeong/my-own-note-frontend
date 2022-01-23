3e4bdec587540094704d89d5f95761ccc4b7e48d
Ji Yeong Seo <tj5134@naver.com>
2022-01-23 10:06:39 +0900

    페이지 추가 버튼 작성

---
+++ src/component/AddPage/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+  isAdditing: boolean;
+  clickAddMode: () => void;
+  enterAddPage: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+`
+
+const Text = styled.p`
+  color: #ffffff;
+  margin: 15px 0px 10px 26px;
+`
+
+const Input = styled.input`
+  width: 150px;
+  height: 25px;
+  margin: 0px 0px 0px 23px;
+  border-radius: 5px;
+  border: 1px solid #0000005f;
+  font-size: 15px;
+  &:focus {
+    outline: none;
+  }
+`
+
+const Image = styled.img`
+  width: 23px;
+  height: 23px;
+  cursor: pointer;
+  margin: 13px 0px 10px 10px;
+`
+
+const AddPage = ({ isAdditing, clickAddMode, enterAddPage }: Props) => {
+  const imageSrc = !isAdditing ? '../../images/add_input_icon.svg' : '../../images/cancel_input_icon.svg';
+  const text = !isAdditing ? '페이지 추가하기' : '닫기';
+  return (
+    <>
+      {isAdditing && <Input onKeyUp={(e) => enterAddPage(e)} />}
+      <Wrapper>
+        <Text>{text}</Text>
+        <Image onClick={() => { clickAddMode() }} src={imageSrc} />
+      </Wrapper>
+    </>
+  )
+}
+
+export default AddPage;
